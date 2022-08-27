943433dc1f0ed145341c2bb019572522a780d73e
Rodrigo Arcova <rodrigo.arcova.external@lojasrenner.com.br>
2022-08-26 22:06:12 -0300

    eslint, prettier, editconfig + org.imports

--- App.tsx
+++ App.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import {SafeAreaView, Text} from 'react-native';
+
 const globalAny: any = global;
 const isHermes = () => String(!!globalAny.HermesInternal);
-const App = () => {
-  return (
-    <SafeAreaView>
-      <Text>Weather Location App</Text>
-      <Text>- Hermes is in use: {isHermes()}</Text>
-    </SafeAreaView>
-  );
-};
+const App = () => (
+	<SafeAreaView>
+		<Text>Weather Location App</Text>
+		<Text>- Hermes is in use: {isHermes()}</Text>
+	</SafeAreaView>
+);
 
 export default App;
